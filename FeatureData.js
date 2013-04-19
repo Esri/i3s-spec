12c6ba4c9f831c4dd17d802380d710e172548719
Thorsten Reitz <treitz@esri.com>
2013-04-19 16:22:08 +0200

    Modified index structure for geometry attributes so that there are independent indices for position, text coord and normals. Removed inconsistencies in accessor objects.

--- FeatureData.js
+++ FeatureData.js
@@ -4,24 +4,21 @@
 			"id": 309432971018, // id is a long value.
 			"mbb": [1222178.283, 83371.902, 54758.098, 2242.100, 34.155, 29.821], // the mbb refines the mbs information that the client already has from the NodeIndexDocument.
 			"classname": "Building", // links this feature to a defined layer.
-			"geometries": [ // Geometry defintion including materials; note thath one Feature can have multiple geometries
+			"geometries": [ // Geometry defintion including materials; note that one Feature can have multiple geometries
 				{
-					"type": "triangles", // types are: triangles, lines, points (i.e. GL render primitives)
+					"type": "triangles", // types are: triangle_strip, triangles, lines, points (i.e. GL render primitives)
 					"components": [ // a single geometry can have multiple components if different materials are used (i.e. glass for windows, brick texture for walls, ...)
 						{
-							"id": 0, // geometry component ID; only locally valid.
 							"material": {
 								"href": "./shared#Mat01", // node-relative URL to access the material. Uses an anchor to define the actual material in the shared resources bundle.
 							}
 						},
 						{
-							"id": 1,
 							"material": {
 								"href": "./51/shared#Mat02", // node-relative URL to access the material. Uses an anchor to define the actual material in the shared resources bundle.
 							}
 						},
 						{
-							"id": 2,
 							"material": {
 								"href": "./51/shared#Mat03", // node-relative URL to access the material. Uses an anchor to define the actual material in the shared resources bundle.
 							}
@@ -31,27 +28,41 @@
 						"position": { 
 							"bufferEntry": 3, // the data block that the required bytes are in.
 							"byteOffset": 254976, // the starting byte position where the required bytes begin.
-							"byteStride": 12, // how many bytes make up a single useable element; here: a vertex position is defined by three 4-byte Float values
-							"count": 344, // the number of elements. Multiply by byteStride to know how many bytes need to be read.
+							"count": 345, // the number of elements. Multiply by byteStride to know how many bytes need to be read.
 							"valueType": "Float32", // the element type, either UInt8, UInt16, Int16, Int32, Int64 or Float32, Float64
 							"valuesPerElement": 3 
 						},
 						"normal": { }, // normals
-						"triangles": { // Triangle indices
-							"bufferEntry": 3,
-							"byteOffset": 165349,
-							"byteStride": 2,
-							"count": 274,
-							"elementType": "Int16",
-							"elementsPerValue": 1,
-							"componentCounts": [
-								42,
-								198,
-								34
-							]
-						},
 						"uv0": { }, // texture coordinates (optional)
-						"classification": { } // per-vertex classification/color/... attributes (optional)
+						"classification": { }, // per-vertex classification/color/... attributes (optional)
+						"faces": { // indices for positions, normals, texture coordinates to build faces
+							"position": {
+								"bufferEntry": 3,
+								"byteOffset": 165349,
+								"count": 345,
+								"valueType": "Int16",
+								"valuesPerElement": 1,
+								"componentIndices": [ // array index where the component starts.
+									0,
+									36,
+									246
+								]
+							},
+							"normal": {
+								"bufferEntry": 3,
+								"byteOffset": 165349,
+								"count": 345,
+								"valueType": "Int16",
+								"valuesPerElement": 1
+							},
+							"uv0": {
+								"bufferEntry": 3,
+								"byteOffset": 165349,
+								"count": 345,
+								"valueType": "Int16",
+								"valuesPerElement": 1
+							}
+						}
 					},
 					"transformation" : [1.0, 0.0, 0.0, 0.0, -0.0, 1.0, 0.0, 0.0, 0.0, -0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0]
 				}

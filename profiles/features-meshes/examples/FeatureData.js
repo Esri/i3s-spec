61aee407f31d4290a39cb6a7c086f75b829c8a5f
Thorsten Reitz <treitz@esri.com>
2014-09-25 11:28:36 +0200

    Removed GeometryComponents from features-meshes rules & examples

--- profiles/features-meshes/examples/FeatureData.js
+++ profiles/features-meshes/examples/FeatureData.js
@@ -17,25 +17,8 @@
 					"params": { 
 						"topology": "PerAttributeArray", // one of {*PerAttributeArray*, InterleavedArray, Indexed}. When "Indexed", the indices must also be declared in the geometry schema and precede the vertexAttribute data.
 						"type": "triangles", // types are: triangle_strip, triangles, lines, points (i.e. GL render primitives)
-						"components": [ // a single geometry can have multiple components, e.g. to assign multiple materials (i.e. glass for windows, brick texture for walls, ...). Not used with LoD aggregate geometries.
-							{
-								"id": 1,
-								"material": "/materialDefinitions/Mat01", // JSON pointer to the defintion of the Material from the root of this node's shared resources  
-								"texture": "/textureDefinitions/38572918", // optional JSON pointer to the texture of the Material from the root of this node's shared resources  
-								"region": 1 // optional ID of the subregion of Map to use with this geometry component
-							},
-							{
-								"id": 2,
-								"material": "/materialDefinitions/Mat01", // JSON pointer to the defintion of the Material from the root of this node's shared resources  
-								"texture": "/textureDefinitions/38572919", // optional JSON pointer to the texture of the Material from the root of this node's shared resources  
-								"region": 1 // optional ID of the subregion of Map to use with this geometry component
-							},
-							{
-								"id": 3,
-								"material": "/materialDefinitions/Mat01", // JSON pointer to the defintion of the Material from the root of this node's shared resources  
-								"texture": "/textureDefinitions/38572920" // optional JSON pointer to the texture of the Material from the root of this node's shared resources  
-							}
-						],
+						"material": "[../shared]/materialDefinitions/Mat01", // JSON Pointer style reference to the material definition (property name) in this node's shared resource, from its root property.
+						"texture": "[../shared]/textureDefinitions/44", // JSON Pointer style reference to the texture definition (position in the array) in this node's shared resource, from its root property.
 						"vertexAttributes": { // these are the vertex attributes. Each attribute is described by an accessor to the geometry typed array. This is an open list.
 							"position": { // the name of the vertex attribute; here: vertex positions
 								"byteOffset": 254976, // the starting byte position where the required bytes begin.
@@ -48,19 +31,7 @@
 								"count": 345, // the number of elements. Multiply by number of bytes used for valueType to know how many bytes need to be read.
 								"valueType": "Int16", // the element type, either UInt8, UInt16, UInt32,  *Int16*, Int32, Int64 or Float32, Float64
 								"valuesPerElement": 2 // number of (Int16) values need to make a valid element (here a normal vector)
-							},
-							"uv0": { // the name of the vertex attribute; here: 1st texture coordinates, must be present if a textureID is referenced
-								"byteOffset": 260496, // the starting byte position where the required bytes begin.
-								"count": 345, // the number of elements. Multiply by number of bytes used for valueType to know how many bytes need to be read.
-								"valueType": "Int16", // the element type, either UInt8, UInt16, UInt32,  *Int16*, Int32, Int64 or Float32, Float64
-								"valuesPerElement": 2 // number of (Int16) values need to make a valid element (here a texture coordinate that will be normalized)
 							}, 
-							"classification": { // the name of the vertex attribute; here: vertex classification/colors
-								"byteOffset": 261876, // the starting byte position where the required bytes begin.
-								"count": 345, // the number of elements. Multiply by number of bytes used for valueType to know how many bytes need to be read.
-								"valueType": "UInt8", // the element type, either *UInt8*, UInt16, UInt32, Int16, Int32, Int64 or Float32, Float64
-								"valuesPerElement": 1 // number of (UInt8) values need to make a valid element (here a classification)
-							},
 							"color": { // vertex colors; must be present if the material property "vertexColors" is set to true 
 								"byteOffset": 263976, // the starting byte position where the required bytes begin.
 								"count": 345, // the number of elements. Multiply by number of bytes used for valueType to know how many bytes need to be read.
@@ -69,11 +40,6 @@
 							}
 						},
 						"faces": { // indices for positions, normals, texture coordinates to build faces. This is an open list.
-							"componentIndices": [ // array index where the component ends (!!)
-									36,
-									245,
-									345
-							],
 							"position": { // position index array buffer view
 								"byteOffset": 165349,
 								"count": 345,
@@ -89,6 +55,57 @@
 							"uv0": { // texture coordinates index array buffer view
 								"byteOffset": 165349,
 								"count": 345,
+								"valueType": "Int16",
+								"valuesPerElement": 1
+							}
+						}
+					}
+				},
+								{
+					"id": 34532534,
+					"type": "ArrayBufferView", // type denotes whether the following geometry is defined by using array buffer views (ArrayBufferView), as a reference to a shared Resource (SharedResourceReference) or embedded (Embedded).
+					"transformation" : [1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0], // linearized 4x4 transformation matrix. Elements 13-15 of the 16 indicate the translational component.
+					"params": { 
+						"topology": "PerAttributeArray", // one of {*PerAttributeArray*, InterleavedArray, Indexed}. When "Indexed", the indices must also be declared in the geometry schema and precede the vertexAttribute data.
+						"type": "triangles", // types are: triangle_strip, triangles, lines, points (i.e. GL render primitives)
+						"material": "[../shared]/materialDefinitions/Mat02", // JSON Pointer style reference to the material definition (property name) in this node's shared resource, from its root property.
+						"texture": "[../shared]/textureDefinitions/45", // JSON Pointer style reference to the texture definition (position in the array) in this node's shared resource, from its root property.
+						"vertexAttributes": { // these are the vertex attributes. Each attribute is described by an accessor to the geometry typed array. This is an open list.
+							"position": { // the name of the vertex attribute; here: vertex positions
+								"byteOffset": 254976, // the starting byte position where the required bytes begin.
+								"count": 121, // the number of elements. Multiply by number of bytes used for valueType to know how many bytes need to be read.
+								"valueType": "Float32", // the element type, either UInt8, UInt16, UInt32,  Int16, Int32, Int64 or *Float32*, Float64
+								"valuesPerElement": 3  // number of (Float32) values need to make a valid element (here a xyz position)
+							},
+							"normal": { // the name of the vertex attribute; here: vertex normals
+								"byteOffset": 259116, // the starting byte position where the required bytes begin.
+								"count": 121, // the number of elements. Multiply by number of bytes used for valueType to know how many bytes need to be read.
+								"valueType": "Int16", // the element type, either UInt8, UInt16, UInt32,  *Int16*, Int32, Int64 or Float32, Float64
+								"valuesPerElement": 2 // number of (Int16) values need to make a valid element (here a normal vector)
+							},
+							"uv0": { // the name of the vertex attribute; here: 1st texture coordinates, must be present if a textureID is referenced
+								"byteOffset": 260496, // the starting byte position where the required bytes begin.
+								"count": 121, // the number of elements. Multiply by number of bytes used for valueType to know how many bytes need to be read.
+								"valueType": "Int16", // the element type, either UInt8, UInt16, UInt32,  *Int16*, Int32, Int64 or Float32, Float64
+								"valuesPerElement": 2 // number of (Int16) values need to make a valid element (here a texture coordinate that will be normalized)
+							}
+						},
+						"faces": { // indices for positions, normals, texture coordinates to build faces. This is an open list.
+							"position": { // position index array buffer view
+								"byteOffset": 265349,
+								"count": 121,
+								"valueType": "Int16",
+								"valuesPerElement": 1
+							},
+							"normal": { // normals index array buffer view
+								"byteOffset": 265349,
+								"count": 121,
+								"valueType": "Int16",
+								"valuesPerElement": 1
+							},
+							"uv0": { // texture coordinates index array buffer view
+								"byteOffset": 265349,
+								"count": 121,
 								"valueType": "Int16",
 								"valuesPerElement": 1
 							}

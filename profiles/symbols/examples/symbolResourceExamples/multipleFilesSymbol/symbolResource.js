77c96ea6e12185d2d174299719906806d5d7dc36
Johannes Schmid <jschmid@esri.com>
2015-03-17 08:06:03 -0700

    Remove geometry type parameter in symbol resource profile, introduce topology parameter instead

--- profiles/symbols/examples/symbolResourceExamples/multipleFilesSymbol/symbolResource.js
+++ profiles/symbols/examples/symbolResourceExamples/multipleFilesSymbol/symbolResource.js
@@ -12,7 +12,7 @@
         "type": "ArrayBufferView", // type denotes whether the following geometry is defined by using array buffer views (ArrayBufferView), as a reference to a shared Resource (SharedResourceReference) or embedded (Embedded).
         "transformation": [1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0], // linearized 4x4 transformation matrix. Elements 13-15 of the 16 indicate the translational component.
         "params": {
-          "type": "triangles", // types are: triangle_strip, triangles, lines, points (i.e. GL render primitives)
+          "topology": "Indexed", // one of ["PerAttributeArray", "Indexed"]. When "Indexed", the indices must also be provided (in "faces"). Otherwise, indices should be left away.
           "material": "/materialDefinitions/Mat01", // JSON Pointer style reference to the material definition in this node's shared resource, from its root element. If present, used for the entire geometry.
           "texture": "/textureDefinitions/Tex01", // JSON Pointer style reference to the texture definition in this node's shared resource, from its root element. If present, used for the entire geometry.
           "vertexAttributes": { // these are the vertex attributes. Each attribute is described by an accessor to the geometry typed array. This is an open list.
